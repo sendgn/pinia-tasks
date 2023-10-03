ad7b3ddf1badb1190f25dc3fdb899aab02c53692
Aleksandr Polozhenskii <polozhenskii@gmail.com>
2023-10-04 01:47:43 +0300

    Add task details and style it

--- src/App.vue
+++ src/App.vue
@@ -3,13 +3,27 @@
     <!-- heading -->
     <header>
       <img src="./assets/pinia-logo.svg" alt="pinia logo">
-      <h1>Pinia tasks</h1>
+      <h1>Pinia Tasks</h1>
     </header>
+    <!-- task list -->
+    <div class="task-list">
+      <div v-for="task in taskStore.tasks">
+        <TaskDetails :task="task" />
+      </div>
+    </div>
   </main>
 </template>
 
 <script>
+  import TaskDetails from './components/TaskDetails.vue'
+  import { useTaskStore } from './stores/TaskStore'
+
   export default {
-    
+    components: { TaskDetails },
+    setup() {
+      const taskStore = useTaskStore()
+
+      return { taskStore }
+    }
   }
 </script>
