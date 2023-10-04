76ae0f45b0b6be80ceda2b2c671c8aa4232dc7c8
Aleksandr Polozhenskii <polozhenskii@gmail.com>
2023-10-04 04:04:17 +0300

    Add filter

--- src/App.vue
+++ src/App.vue
@@ -5,9 +5,33 @@
       <img src="./assets/pinia-logo.svg" alt="pinia logo">
       <h1>Pinia Tasks</h1>
     </header>
+    <!-- filter -->
+    <nav class="filter">
+      <p>
+        Count:
+        <span>
+          {{ filter === 'all' ? taskStore.totalCount : taskStore.favCount }}
+        </span>
+      </p>
+      <div>
+        <button
+          @click="filter = 'all'"
+          :class="{ active: filter === 'all' }"
+        >All tasks</button>
+        <button
+          @click="filter = 'favs'"
+          :class="{ active: filter === 'favs' }"
+        >Fav tasks</button>
+      </div>
+    </nav>
     <!-- task list -->
-    <div class="task-list">
+    <div class="task-list" v-if="filter === 'all'">
       <div v-for="task in taskStore.tasks">
+        <TaskDetails :task="task" />
+      </div>
+    </div>
+    <div class="task-list" v-if="filter === 'favs'">
+      <div v-for="task in taskStore.favs">
         <TaskDetails :task="task" />
       </div>
     </div>
@@ -15,6 +39,7 @@
 </template>
 
 <script>
+  import { ref } from 'vue'
   import TaskDetails from './components/TaskDetails.vue'
   import { useTaskStore } from './stores/TaskStore'
 
@@ -23,7 +48,9 @@
     setup() {
       const taskStore = useTaskStore()
 
-      return { taskStore }
+      const filter = ref('all')
+
+      return { taskStore, filter }
     }
   }
 </script>
--- src/stores/TaskStore.js
+++ src/stores/TaskStore.js
@@ -6,5 +6,16 @@ export const useTaskStore = defineStore('taskStore', {
       { id: 1, title: 'buy some milk', isFav: false },
       { id: 2, title: 'play Heavy Rain', isFav: true }
     ]
-  })
+  }),
+  getters: {
+    favs() {
+      return this.tasks.filter(t => t.isFav)
+    },
+    favCount() {
+      return this.tasks.reduce((acc, cur) => {
+        return cur.isFav ? acc + 1 : acc
+      }, 0)
+    },
+    totalCount: (state) => state.tasks.length
+  }
 })
